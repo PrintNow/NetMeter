59c168cde3c781aaf94816c37217ef1a3152eff3
Shine <i@nowtime.cc>
2026-04-11 01:37:36 +0800

    Initial Commit

---
+++ NetMeter/NetMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetMeterApp.swift
+//  NetMeter
+//
+//  Created by Shine on 2026/4/11.
+//
+
+import SwiftUI
+
+@main
+struct NetMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
