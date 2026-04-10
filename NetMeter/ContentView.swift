8856d50a320881c9290dacf481395ea926ac2ecb
Shine <i@nowtime.cc>
2026-04-11 01:37:36 +0800

    Initial Commit

---
+++ NetMeter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetMeter
+//
+//  Created by Shine on 2026/4/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
